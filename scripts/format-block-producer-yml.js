e8ee16f9309a7a52cb7a580912351bcc300bce4e
Denis <carriere.denis@gmail.com>
2018-04-12 16:18:05 -0400

    Format Block Producers

--- scripts/format-block-producer-yml.js
+++ scripts/format-block-producer-yml.js
@@ -12,11 +12,14 @@ glob.sync(path.join(__dirname, '..', 'block-producers', '*.yml')).forEach(filepa
   fs.writeFileSync(filepath, `# EOS Scholar - Block Producer
 # https://github.com/ScholarTestnet
 
-# EOS Accounts (Required)
+# Developer Account (Required)
 account_name: ${config.account_name}
 owner_public_key: ${config.owner_public_key}
 active_public_key: ${config.active_public_key}
-initial_block_signing_key: ${config.initial_block_signing_key}
+
+# Block Producer Account (Required)
+producer_name: ${config.producer_name}
+block_signing_key: ${config.block_signing_key}
 
 # Primary Contact (Required)
 telegram_user: ${config.telegram_user}
@@ -30,7 +33,7 @@ agent_name: ${config.agent_name || ''}
 
 # Organization (Optional)
 organization_name: ${config.organization_name || ''}
-logo_url: ${config.logo || ''}
+logo_url: ${config.logo_url || ''}
 timezone: ${config.timezone || ''}
 website: ${config.website || ''}
 `)
