283fcbbbcb78ec82560ecac04ea9374a539bb8ca
Denis <carriere.denis@gmail.com>
2018-04-23 09:10:14 -0400

    Add connectivity test

---
+++ scripts/test-bp-connectivity.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+const glob = require('glob');
+const axios = require('axios').default;
+
+glob.sync(path.join(__dirname, '..', 'block-producers', 'scholar-testnet', '*.yml')).forEach(async filepath => {
+  const config = yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
+  const url = `https://${config.domain}:${config.http}/v1/chain/get_info`
+  const response = await axios.get(url, {timeout: 2000}).catch(() => console.log('fail', url))
+  console.log('pass', url)
+})
