db46acf12653de334eb5e82ac2a45c8176dac6df
Denis <carriere.denis@gmail.com>
2018-04-24 15:26:15 -0400

    Update generate producer-list

--- scripts/generate-producer-list.js
+++ scripts/generate-producer-list.js
@@ -15,50 +15,78 @@ let currentFileIndex = 0;
 
 glob.sync(path.join(__dirname, '..', 'block-producers', folder, '*.yml')).forEach((filepath, index, files) => {
   const config = yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
-  const {organization_name, website, producer_name, logo_url, domain, http, p2p, telegram_user, keybase} = config;
 
+  let {organization_name, longitude, latitude, website, eosio_account_name, logo_url, eosio_https_host, eosio_https_port, eosio_p2p_port, social_telegram, social_keybase} = config;
+
+  // Rename to Discovery schema
+  const producer_name = eosio_account_name
+  const domain = eosio_https_host
+  const http = eosio_https_port
+  const p2p = eosio_p2p_port
+  const telegram_user = social_telegram
+  const keybase = social_keybase
+
+  // Default lat & lng
+  longitude = longitude || 0.0
+  latitude = latitude || 0.0
+
+  // Defaults
   const defaultLogo = "https://user-images.githubusercontent.com/550895/37975477-6e1b53dc-31ad-11e8-9367-3415c3bf29b9.png";
-
-  if (domain) {
-
-    // query ip address
-    dns.lookup(domain, function (err, result) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      console.log('generating ' + organization_name);
-
-      // query latlng from ipstack.com
-      request('http://api.ipstack.com/' + result + '?access_key=2edfb3d626b2886fa93ce3b0d0684c89', function (error, response, body) {
-        if (error) {
-          console.log(error);
-          return;
-        }
-        body = JSON.parse(body);
-        if (response && response.statusCode === 200) {
-          currentFileIndex++;
-          producerList.push({
-            // TODO, Include EOS Public Key for producers
-            "logo": `${logo_url || defaultLogo}`,
-            "producer": `${producer_name || ""}`,
-            "organization_name": `${organization_name || ""}`,
-            "website": `${website || ""}`,
-            "telegram_user": `${telegram_user || ""}`,
-            "API_URL": `${domain || ""}`,
-            "HTTP": `${http || ""}`,
-            "P2P": `${p2p || ""}`,
-            "keybase": `${keybase || ""}`,
-            "LngLat": [body.longitude, body.latitude]
-          });
-
-          if (currentFileIndex === files.length) {
-            process.stdout.write(JSON.stringify(producerList, null, 2) + '\n');
-          }
-        }
-      });
-
-    })
-  }
+  producerList.push({
+    // TODO, Include EOS Public Key for producers
+    "logo": `${logo_url || defaultLogo}`,
+    "producer": `${producer_name || ""}`,
+    "organization_name": `${organization_name || ""}`,
+    "website": `${website || ""}`,
+    "telegram_user": `${telegram_user || ""}`,
+    "API_URL": `${domain || ""}`,
+    "HTTP": `${http || ""}`,
+    "P2P": `${p2p || ""}`,
+    "keybase": `${keybase || ""}`,
+    "LngLat": [longitude, latitude]
+  })
 });
+
+process.stdout.write(JSON.stringify(producerList, null, 2) + '\n');
+  // if (domain) {
+
+  //   // query ip address
+  //   dns.lookup(domain, function (err, result) {
+  //     if (err) {
+  //       console.log(err);
+  //       return;
+  //     }
+
+  //     process.stderr.write(`generating ${organization_name}\n`);
+
+  //     // query latlng from ipstack.com
+  //     request('http://api.ipstack.com/' + result + '?access_key=2edfb3d626b2886fa93ce3b0d0684c89', function (error, response, body) {
+  //       if (error) {
+  //         console.log(error);
+  //         return;
+  //       }
+  //       body = JSON.parse(body);
+  //       if (response && response.statusCode === 200) {
+  //         currentFileIndex++;
+  //         producerList.push({
+  //           // TODO, Include EOS Public Key for producers
+  //           "logo": `${logo_url || defaultLogo}`,
+  //           "producer": `${producer_name || ""}`,
+  //           "organization_name": `${organization_name || ""}`,
+  //           "website": `${website || ""}`,
+  //           "telegram_user": `${telegram_user || ""}`,
+  //           "API_URL": `${domain || ""}`,
+  //           "HTTP": `${http || ""}`,
+  //           "P2P": `${p2p || ""}`,
+  //           "keybase": `${keybase || ""}`,
+  //           "LngLat": [body.longitude, body.latitude]
+  //         });
+  //         process.stdout.write(JSON.stringify(producerList, null, 2) + '\n');
+
+  //         // if (currentFileIndex === files.length) {
+  //         // }
+  //       }
+  //     });
+  //   })
+//   })
+// });
